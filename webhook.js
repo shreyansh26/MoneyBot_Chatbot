b60e98407dc806219c7325ec1b5f85785704460c
Shreyansh <shreyansh.pettswood@gmail.com>
2017-06-06 17:30:56 +0530

    Currency conversion method added

--- webhook.js
+++ webhook.js
@@ -5,10 +5,35 @@ const server = Restify.createServer({
   name: "MoneyBot"
 });
 
+const request = require('request');
 const PORT = process.env.PORT || 3000;
 
 server.use(Restify.bodyParser());
 server.use(Restify.jsonp());
+
+const convertCurrency = (amountToConvert, outputCurrency, cb) => {
+  const {
+    amount,
+    currency
+  } = amountToConvert;
+  return request({
+    uri: "https://api.fixer.io/latest",
+    qs: {
+      base: currency,
+      symbols: outputCurrency
+    },
+    method: 'GET',
+    json: true
+  }, (error, response, body) => {
+    if(!error && response.statusCode === 200) {
+      let computedValue = Math.round(body.rates[outputCurrency] * amount);
+      cb(null, `${amount} ${currency} converts to about ${outputCurrency} ${computedValue} as per current rates!`);
+    } else {
+      cb(error, null);
+    }
+  });
+
+}
 
 // Post route handler
 server.post('/', (req, res, next) => {
@@ -39,7 +64,15 @@ server.post('/', (req, res, next) => {
       });
     } else {
       // Query the fixer.io API to fetch response
-      
+      convertCurrency(amountToConvert, outputCurrency, (error, result) => {
+        if(!error && result) {
+          res.json({
+            speech: result,
+            displayText: result,
+            source: "moneybot-webhook"
+          });
+        }
+      });
     }
   }
   console.log(result);
